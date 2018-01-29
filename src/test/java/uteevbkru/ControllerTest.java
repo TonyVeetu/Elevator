b552639aa4581a091c6abe11d6469a46855b40ac
Tony Uteev <uteev@bk.ru>
2018-01-29 18:09:22 +0300

    ControllerTest

--- src/test/java/uteevbkru/ControllerTest.java
+++ src/test/java/uteevbkru/ControllerTest.java
@@ -1,19 +1,21 @@
 package uteevbkru;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-public class ElevatorTest {
+public class ControllerTest {
     private ElevatorOverTheGround elevator;
-    private AtomicBoolean isIterable = new AtomicBoolean(false);
+    private Controller controller;
+    private AtomicBoolean isIterable;
     private BlockingQueue<Integer> queueOfFloors;
 
-    @org.junit.Before
-    public void setUp() {
+    @Before
+    public void setUp(){
         int countOfFloors = 10;
         double speed = 2.0;
         double floorHeight = 2.0;
@@ -22,24 +24,22 @@ public class ElevatorTest {
         int capacityOfQueue = countOfFloors;//Не может очередь быть больше количества этажей в подьезде!
         queueOfFloors = new ArrayBlockingQueue<>(capacityOfQueue);
         elevator = new ElevatorOverTheGround(countOfFloors, speed, floorHeight, gapOpenClose, queueOfFloors, isIterable);
-        queueOfFloors.add(5);
-    }
-
-    @org.junit.After
-    public void tearDown() throws Exception {
+        isIterable = new AtomicBoolean();
+        controller = new Controller(elevator, queueOfFloors, isIterable);
     }
 
     @Test
-    public void getTimeForOneFloorTest(){
-        Assert.assertEquals(1000, elevator.getTimeForOneFloor());
+    public void getFloorTest() {
+        String strFloor = "3";
+        int floor = controller.getFloor(strFloor);
+        Assert.assertEquals(3, floor);
     }
 
     @Test
-    public void getCountOfFloorTest(){
-        Assert.assertEquals(5, elevator.getCountOfFloors(5));
+    public void getFloorErrorTest(){
+        String strFloor = "a";
+        int floor = controller.getFloor(strFloor);
+        Assert.assertEquals(0, floor);
     }
-
-    @Test
-    public void getNextFloorTest(){Assert.assertEquals(5,elevator.getNextFloor());}
 
 }
